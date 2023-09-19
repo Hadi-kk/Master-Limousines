a5366ab056685765756cc3d6dec9bb4c9d19bdea
Hadi-kk <hadinioh@gmail.com>
2023-09-19 06:59:59 +0500

    logo onclick added and nave bar hide on scroll

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,91 +1,114 @@
 "use client";
 import { useState, useEffect } from "react";
-import { AppBar, Button, Grid, Tab, Tabs } from "@mui/material";
+import {
+    AppBar,
+    Button,
+    Grid,
+    Slide,
+    Tab,
+    Tabs,
+    useScrollTrigger,
+} from "@mui/material";
 import Link from "next/link";
 import logo from "@/assets/svgs/Logo.svg";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+
+function HideOnScroll({ children }: any) {
+    const trigger = useScrollTrigger();
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+}
 
 const Navbar = () => {
     const [value, setValue] = useState("");
     const pathname = usePathname();
+    const router = useRouter();
 
     const handleChange = (event: React.SyntheticEvent, newValue: string) => {
         setValue(newValue);
     };
 
     useEffect(() => {
-        console.log("here is path", pathname);
+        router.prefetch("/");
         setValue(pathname);
     }, [pathname]);
 
     return (
-        <AppBar color="transparent" sx={{ p: 4, boxShadow: "none" }}>
-            <Grid container justifyContent="space-between" direction="row">
-                <Grid item>
-                    <Image
-                        src={logo}
-                        alt="Master Limousines"
-                        width={200}
-                        height={48}
-                    />
-                </Grid>
-                <Grid item>
-                    <Tabs
-                        value={value}
-                        onChange={handleChange}
-                        indicatorColor="secondary"
-                        textColor="inherit"
-                        aria-label="Navigation tabs"
-                    >
-                        <Tab
-                            value="/"
-                            label="HOME"
-                            href="/"
-                            LinkComponent={Link}
+        <HideOnScroll>
+            <AppBar color="transparent" sx={{ p: 4, boxShadow: "none" }}>
+                <Grid container justifyContent="space-between" direction="row">
+                    <Grid item>
+                        <Image
+                            src={logo}
+                            alt="Master Limousines"
+                            width={200}
+                            height={48}
+                            onClick={() => router.push("/")}
+                            className="image-with-shadow"
                         />
-
-                        <Tab
-                            value="/services"
-                            label="SERVICES"
-                            href="/services"
-                            LinkComponent={Link}
-                        />
-
-                        <Tab
-                            value="/events"
-                            label="EVENTS"
-                            href="/events"
-                            LinkComponent={Link}
-                        />
-
-                        <Tab
-                            value="/fleet"
-                            label="OUR FLEET"
-                            href="/fleet"
-                            LinkComponent={Link}
-                        />
-
-                        <Tab
-                            value="/about"
-                            label="ABOUT US"
-                            href="/about"
-                            LinkComponent={Link}
-                        />
-
-                        <Button
-                            value="/reservations"
-                            variant="contained"
-                            color="primary"
-                            href="/reservations"
-                            LinkComponent={Link}
+                    </Grid>
+                    <Grid item>
+                        <Tabs
+                            value={value}
+                            onChange={handleChange}
+                            indicatorColor="secondary"
+                            textColor="inherit"
+                            aria-label="Navigation tabs"
                         >
-                            RESERVATIONS
-                        </Button>
-                    </Tabs>
+                            <Tab
+                                value="/"
+                                label="HOME"
+                                href="/"
+                                LinkComponent={Link}
+                            />
+
+                            <Tab
+                                value="/services"
+                                label="SERVICES"
+                                href="/services"
+                                LinkComponent={Link}
+                            />
+
+                            <Tab
+                                value="/events"
+                                label="EVENTS"
+                                href="/events"
+                                LinkComponent={Link}
+                            />
+
+                            <Tab
+                                value="/fleet"
+                                label="OUR FLEET"
+                                href="/fleet"
+                                LinkComponent={Link}
+                            />
+
+                            <Tab
+                                value="/about"
+                                label="ABOUT US"
+                                href="/about"
+                                LinkComponent={Link}
+                            />
+
+                            <Button
+                                value="/reservations"
+                                variant="contained"
+                                color="primary"
+                                href="/reservations"
+                                LinkComponent={Link}
+                            >
+                                RESERVATIONS
+                            </Button>
+                        </Tabs>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </AppBar>
+            </AppBar>
+        </HideOnScroll>
     );
 };
 
